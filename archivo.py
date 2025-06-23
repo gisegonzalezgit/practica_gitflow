fc89b4090c76685cb5da99810ae23779a00d3cac
gisegonzalezgit <gise.gonzalez144@gmail.com>
2025-06-23 06:16:06 -0300

    feature/nuevaFuncionalidad cambios

--- archivo.py
+++ archivo.py
@@ -1,1 +1,9 @@
 print("archivo")
+#creo variables de prueba
+
+nombre="Juan"
+edad=30
+# creo una lista de prueba
+lista=[1,2,3,4,5]
+# creo un diccionario de prueba
+diccionario={"clave1":"valor1", "clave2":"valor2"}
---
+++ archivo_1.py
@@ -0,0 +1,1 @@
+print("archivo_1")
